4e49d0a8bdcefa9807fa2d1c762cbad9a11c19b7
David Tudury <david.tudury@voxer.com>
2014-06-15 01:33:28 -0700

    us census data

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
+var last = require('./etc/json/all.last.json');
+var female = require('./etc/json/female.first.json');
+var male = require('./etc/json/male.first.json');
+var places = require('./etc/json/places.json');
 
-var names = require('./names.json')
-var first = require('./first-names.json')
-var middle = require('./middle-names.json')
-var place = require('./places.json')
+function _expand_by_weight(data, atomic) {
+  var expanded = [];
+  data.forEach(function (row) {
+    for (var i = 0; i < Math.round(row.weight / atomic); i++) {
+      expanded.push(row.name);
+    }
+  });
+  return expanded;
+}
+
+var last_weighted = _expand_by_weight(last, 0.0005);
+var female_weighted = _expand_by_weight(female, 0.001);
+var male_weighted = _expand_by_weight(male, 0.001);
+var places_weighted = _expand_by_weight(places, 50000);
+
+var chance_of_middle = 0.75;
+var gender_ratio = 1 - 0.97 / 1.97;
 
 function r(names) {
   return function () {
@@ -10,19 +27,35 @@ function r(names) {
   }
 }
 
-var random = module.exports = function () {
-  return random.first() + ' ' +random.last()
+
+var random = module.exports = function random_person() {
+  var person = {};
+  var name_generator = Math.random() < gender_ratio ? random.female : random.male;
+  person.first = name_generator();
+  if (Math.random() < chance_of_middle) {
+    do {
+      person.middle = name_generator();
+    } while (person.first === person.middle);
+  }
+  person.last = random.last();
+  person.place = random.place();
+  person.toString = function() {
+    return '"' + this.first + (this.middle ? ' ' + this.middle : '') +  ' ' + this.last + '"' + " from " + this.place;
+  }
+  return person;
 }
 
-random.first   = r(first)
-random.last    = r(names)
-random.middle  = r(middle)
-random.place   = r(place)
+random.last   = r(last_weighted);
+random.female = r(female_weighted);
+random.male   = r(male_weighted);
+random.place  = r(places_weighted);
+random.first  = random.middle = function () {
+  return Math.random() < gender_ratio ? random.female() : random.male();
+}
+  
 
 if(!module.parent) {
   var l = process.argv[2] || 10
   while (l--)
-    console.log(random.first(), '.', random.middle(), '.', random.last()
-    , ',', random.place())
+    console.log(random().toString());
 }
-  
--- toJSON.js
+++ toJSON.js
@@ -4,5 +4,26 @@ var JSONStream = require('JSONStream')
 
 process.stdin  
   .pipe(es.split(/\r?\n/))
+  .pipe(es.map(function (data, callback) {
+    var cols;
+    if (cols = data.match(/([A-Z]*)  *([0-9.]*) ([0-9.]*)  *(\d*)/)) {
+      return callback(null, {
+        name: cols[1].charAt(0) + cols[1].substr(1).toLowerCase(),
+        weight: Math.max(parseFloat(cols[2]), 0.0005)
+      });
+    }
+    if (cols = data.match(/0100000US,,United States,.*United States - ([^,\/(-]*) *(?:city|town|village|municipality|CDP).*, ([^"]*).*,(\d*),/)) {
+      return callback(null, {
+        name: cols[1].trim() + ", " + cols[2],
+        weight: parseInt(cols[3])
+      });
+    }
+    if (cols = data.match(/0100000US,,United States,.*United States - ([^,\/(-]*).*, ([^"]*).*,(\d*),/)) {
+      return callback(null, {
+        name: cols[1].trim() + ", " + cols[2],
+        weight: parseInt(cols[3])
+      });
+    }
+  }))
   .pipe(JSONStream.stringify())
   .pipe(process.stdout)
